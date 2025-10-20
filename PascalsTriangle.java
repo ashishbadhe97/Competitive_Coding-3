042ae461f272af9763405fd5985b516fc85e4f30
ashish-badhe <badhe.a@northeastern.edu>
2025-10-20 11:17:54 -0400

    Pascals Triangle

---
+++ PascalsTriangle.java
@@ -0,0 +1,37 @@
+// 118. Pascal's Triangle
+// https://leetcode.com/problems/pascals-triangle/description/
+
+/**
+ * Time Complexity: O(n * m) ~ O(n^2) since we iterate over each element
+ * Space Complexity: O(1) since no auxillary space as we dont consider result 
+ */
+
+
+
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        for(int i = 2 ; i < numRows ; i++){
+
+            List<Integer> currList = new ArrayList<>();
+
+            for(int j = 0 ; j <= i ; j++){
+
+                if(j == 0 || j == i){ // if first or last index , fill with 1
+                    currList.add(1);
+                }else{
+                    List<Integer> prevList = result.get(i - 1);
+
+                    int currNumber = prevList.get(j - 1) + prevList.get(j); // else add previous list elements
+
+                    currList.add(currNumber);
+                }
+            }
+
+            result.add(currList);
+        }
+
+        return result;
+    }
+}
